edc0d0a96456791bfcdcc172c7947126ae5a802e
谢亚希 <yaxi9204@126.com>
2023-08-04 16:56:46 +0800

    '网易云音乐标题搜索'

---
+++ component/musicHeade/musicHeade.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="musichead">
+		<view class="musicIcon" v-if="showIcon" showIcon>
+			<text class="iconfont icon-zuojiantou" @click="back"></text> |  <text class="iconfont icon-shouye" @click="backHome"></text>
+		</view>
+		<view class="musicTitle">{{title}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			title:{
+				type:String,
+				default:''
+			},
+			showIcon:{
+				type:Boolean,
+				default:false
+			}
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			back(){
+				uni.navigateTo();
+			},
+			backHome(){
+				uni.navigateTo({
+					url: '/pages/index/index'
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+.musichead{
+	 width:100%;
+	 height:75px;
+	 line-height:80px;
+	 /* background:rgba(0,0,255,.4); */
+	
+	 position:relative;
+}
+.musicTitle{
+	text-align:center;
+	 color:#000;
+}
+.musicIcon{
+	position: absolute;
+	top:23px;
+	left:15px;
+	padding:0px 15px;
+	height:30px;
+	line-height:30px;
+	background:#b6b3b4;
+	border-radius: 15px;
+	color:#fff;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,133 @@
+<template>
+	<view class="content">
+		<musicHead :title="title" :showIcon="showIcon"></musicHead>
+		<view class="indexContent">
+			<scroll-view scroll-y='true'>
+				<!-- 搜索 -->
+				<view class="indexSearch">
+					<text class="iconfont icon-fangdajing"></text>
+					<input type="text" placeholder="搜索歌曲">
+				</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+				<view>ssssssssss</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import '@/common/iconfont.css'
+	import musicHead from '../../component/musicHeade/musicHeade.vue'
+	export default {
+		components:{musicHead},
+		data() {
+			return {
+				title: '网易云音乐',
+				showIcon:false
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	.indexContent{
+		width:100%;
+		height:calc(100vh - 75px);
+		overflow: scroll;
+	}
+	.indexSearch{
+		margin:0 30upx;
+		background:#F7F7F7;
+		height:60upx;
+		line-height:60upx;
+		border-radius: 30upx;
+		display:flex;
+		padding-left:10px;
+	}
+	.indexSearch>text{
+		width:30upx;
+		height:30upx;
+	}
+	.indexSearch>input{
+		height:60upx;
+		line-height:60upx;
+		margin-left:8px;
+	}
+</style>
